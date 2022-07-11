592d2436406a54279cda99922ac9c6fc58ef699b
cisco <sovisrushain@gmail.com>
2022-07-11 11:28:57 +0530

    crud 03 - update 01

---
+++ 03-crud/src/main/java/com/cisco/UpdateDemoOne.java
@@ -0,0 +1,20 @@
+package com.cisco;
+
+
+import com.cisco.entity.Student;
+import com.cisco.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class UpdateDemoOne {
+
+    public static void main(String[] args) {
+        try(SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+            Session session = sessionFactory.openSession()){
+            session.beginTransaction();
+            Student pathum = session.get(Student.class, 2);
+            pathum.setContactNumber("0711234568");
+            session.getTransaction().commit();
+        }
+    }
+}
